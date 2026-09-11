a5a8a8978e6554c18768ea6e4108260061621428
genstogata <afiliputti@posteo.net>
2026-09-11 17:59:11 -0400

    Add lock/PIN control for toolbar buttons with keypad entry; bump version to v28

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump this on every deploy so clients pick up new files instead of stale cache.
-const CACHE_NAME = "tappy-cache-v27";
+const CACHE_NAME = "tappy-cache-v28";
 const PRECACHE_URLS = [
   "./",
   "./index.html",
