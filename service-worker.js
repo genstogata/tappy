64823034b4b5c2b49f59a2de5a6a7bed4090bf96
genstogata <afiliputti@posteo.net>
2026-09-18 11:31:47 -0400

    Bump version to v33

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump this on every deploy so clients pick up new files instead of stale cache.
-const CACHE_NAME = "tappy-cache-v32";
+const CACHE_NAME = "tappy-cache-v33";
 const PRECACHE_URLS = [
   "./",
   "./index.html",
